99555c84ca17ce14d5ea5ed98d34a2b9c5c47036
Dmitrii Panov <15minstan@gmail.com>
2025-03-16 18:09:55 +0100

    django final project

--- _project/urls.py
+++ _project/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('django_fp/', include('django_fp.urls', namespace='django_fp')),
 
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
