7c08e0a8a501173a3cb6d92c96f79e89ce18d10d
Dmitrii Panov <15minstan@gmail.com>
2025-03-26 23:25:15 +0100

    django final project

--- django_fp/forms.py
+++ django_fp/forms.py
@@ -1,14 +1,15 @@
 from django import forms
 from django.forms import ModelForm
-from django_fp.models import Item
+from django_fp import models
 
 class ItemForm(ModelForm):
     class Meta:
-        model = Item
-        fields = ['name', 'desc', 'actors', 'title']
+        model = models.Item
+        fields = ['name', 'desc', 'actors', 'title', 'type']
         widgets = {
             'name': forms.Textarea(attrs={'rows': 1}),
             'desc': forms.Textarea(attrs={'rows': 2}),
             'actors': forms.SelectMultiple(),
             'title': forms.Textarea(attrs={'rows': 1}),
+            'type': forms.Select(choices=models.ItemType),
         }
