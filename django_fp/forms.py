627acd99d8c77649f956e075a7f675b6259e07b0
Dmitrii Panov <15minstan@gmail.com>
2025-03-26 23:22:22 +0100

    django final project

--- django_fp/forms.py
+++ django_fp/forms.py
@@ -5,8 +5,10 @@ from django_fp.models import Item
 class ItemForm(ModelForm):
     class Meta:
         model = Item
-        fields = ['name', 'desc']
+        fields = ['name', 'desc', 'actors', 'title']
         widgets = {
             'name': forms.Textarea(attrs={'rows': 1}),
-            'desc': forms.Textarea(attrs={'rows': 4})
+            'desc': forms.Textarea(attrs={'rows': 2}),
+            'actors': forms.SelectMultiple(),
+            'title': forms.Textarea(attrs={'rows': 1}),
         }
