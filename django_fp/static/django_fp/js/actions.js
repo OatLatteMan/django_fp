b7ade4694e58ff99ceef8e2cd2e0faad006935c0
Dmitrii Panov <15minstan@gmail.com>
2025-04-14 23:05:48 +0200

    django main project

---
+++ django_fp/static/django_fp/js/actions.js
@@ -0,0 +1,15 @@
+function confirmDelete(url) {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to undo this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#d33',
+        cancelButtonColor: '#3085d6',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            window.location.href = url;
+        }
+    });
+}
