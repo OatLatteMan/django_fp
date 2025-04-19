f474aea306179abd2a8f90590910f59fd1345abd
Dmitrii Panov <15minstan@gmail.com>
2025-04-19 23:15:37 +0200

    django main project
    user profile feature implementing

--- django_fp/models.py
+++ django_fp/models.py
@@ -1,8 +1,18 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.shortcuts import reverse
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 User = get_user_model()
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.SET_NULL)
+    avatar = models.ImageField(blank=True, null=True, upload_to='avatars/')
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
 
 class ItemType(models.IntegerChoices):
     MOVIE = 1, 'Film'
