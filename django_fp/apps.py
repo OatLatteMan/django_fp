3ac719f2549ac5245aaabd31c3b755cb007374ae
Dmitrii Panov <15minstan@gmail.com>
2025-04-23 00:23:12 +0200

    django main project
    moving signals and corresponding functions into separate signals.py file. Connecting signals in apps.py

--- django_fp/apps.py
+++ django_fp/apps.py
@@ -5,3 +5,6 @@ class FinalProjectConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'django_fp'
 
+    def ready(self):
+        import django_fp.signals
+
--- django_fp/models.py
+++ django_fp/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.shortcuts import reverse
-from django.db.models.signals import post_save, post_delete
-from django.dispatch import receiver
+# from django.db.models.signals import post_save, post_delete
+# from django.dispatch import receiver
 
 User = get_user_model()
 
@@ -63,27 +63,27 @@ class Actor(models.Model):
     def __str__(self):
         return self.name
 
-@receiver(post_save, sender=User)
-def create_or_update_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-    else:
-        instance.profile.save()
+# @receiver(post_save, sender=User)
+# def create_or_update_user_profile(sender, instance, created, **kwargs):
+#     if created:
+#         Profile.objects.create(user=instance)
+#     else:
+#         instance.profile.save()
 
-def update_item_average_rating(item):
-    reviews = item.reviews.all()
-    if reviews.exists():
-        avg = sum([r.rate for r in reviews]) / reviews.count()
-    else:
-        avg = 0.0
-    item.average_rating = avg
-    item.save()
+# def update_item_average_rating(item):
+#     reviews = item.reviews.all()
+#     if reviews.exists():
+#         avg = sum([r.rate for r in reviews]) / reviews.count()
+#     else:
+#         avg = 0.0
+#     item.average_rating = avg
+#     item.save()
 
-@receiver(post_save, sender=Review)
-def recalculate_rating_on_save(sender, instance, **kwargs):
-    update_item_average_rating(instance.item)
+# @receiver(post_save, sender=Review)
+# def recalculate_rating_on_save(sender, instance, **kwargs):
+#     update_item_average_rating(instance.item)
 
-@receiver(post_delete, sender=Review)
-def recalculate_rating_on_delete(sender, instance, **kwargs):
-    update_item_average_rating(instance.item)
+# @receiver(post_delete, sender=Review)
+# def recalculate_rating_on_delete(sender, instance, **kwargs):
+#     update_item_average_rating(instance.item)
 
