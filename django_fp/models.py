10c5438547fad69537a9ad30bc77917f6c9e6b3c
Dmitrii Panov <15minstan@gmail.com>
2025-04-23 23:33:09 +0200

    django main project
    custom querysets

--- django_fp/models.py
+++ django_fp/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.shortcuts import reverse
-# from django.db.models.signals import post_save, post_delete
-# from django.dispatch import receiver
 
 User = get_user_model()
 
@@ -51,6 +49,9 @@ class Item(models.Model):
     def get_absolute_url(self):
         return reverse("django_fp:item_detail", kwargs={"pk": self.pk})
 
+class ItemQuerySet(models.QuerySet):
+    pass
+
 class Actor(models.Model):
     film = models.ForeignKey(Item, null=True, on_delete=models.SET_NULL, related_name='actor_film')
     name = models.CharField(max_length=40)
@@ -62,28 +63,4 @@ class Actor(models.Model):
 
     def __str__(self):
         return self.name
-
-# @receiver(post_save, sender=User)
-# def create_or_update_user_profile(sender, instance, created, **kwargs):
-#     if created:
-#         Profile.objects.create(user=instance)
-#     else:
-#         instance.profile.save()
-
-# def update_item_average_rating(item):
-#     reviews = item.reviews.all()
-#     if reviews.exists():
-#         avg = sum([r.rate for r in reviews]) / reviews.count()
-#     else:
-#         avg = 0.0
-#     item.average_rating = avg
-#     item.save()
-
-# @receiver(post_save, sender=Review)
-# def recalculate_rating_on_save(sender, instance, **kwargs):
-#     update_item_average_rating(instance.item)
-
-# @receiver(post_delete, sender=Review)
-# def recalculate_rating_on_delete(sender, instance, **kwargs):
-#     update_item_average_rating(instance.item)
 
